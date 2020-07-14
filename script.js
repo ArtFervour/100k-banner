851666736f55789c0fab2e10922cbe25372ea5ff
Suraj Mandal <surajmandalcell@gmail.com>
2020-07-14 20:35:47 +0530

    init

---
+++ script.js
@@ -0,0 +1,17 @@
+tippy('.btn-softcover', {
+  html: '#softcover-book', 
+  arrow: true,
+  animation: 'shift',
+  size: 'regular',
+  theme: 'au',
+  distance: 15,
+})
+
+tippy('.btn-hardcover', {
+  html: '#hardcover-book', 
+  arrow: true,
+  animation: 'shift',
+  size: 'regular',
+  theme: 'au',
+  distance: 15,
+})
