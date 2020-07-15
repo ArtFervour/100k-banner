306c30c13f1fc6a53f635ba38948e038d62f215a
Suraj Mandal <surajmandalcell@gmail.com>
2020-07-15 10:21:28 +0530

    change positions

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
-tippy('.btn-softcover', {
-  html: '#softcover-book', 
+tippy(".btn-softcover", {
+  html: "#softcover-book",
   arrow: true,
-  animation: 'shift',
-  size: 'regular',
-  theme: 'au',
+  animation: "shift",
+  size: "regular",
+  theme: "au",
   distance: 15,
-})
+});
 
-tippy('.btn-hardcover', {
-  html: '#hardcover-book', 
+tippy(".btn-fresh", {
+  html: "#fresh-book",
   arrow: true,
-  animation: 'shift',
-  size: 'regular',
-  theme: 'au',
+  animation: "shift",
+  size: "regular",
+  theme: "au",
   distance: 15,
-})
+});
+
+tippy(".btn-persona", {
+  html: "#persona-book",
+  arrow: true,
+  animation: "shift",
+  size: "regular",
+  theme: "au",
+  distance: 15,
+});
+
+tippy(".btn-games", {
+  html: "#games-book",
+  arrow: true,
+  animation: "shift",
+  size: "regular",
+  theme: "au",
+  distance: 15,
+});
+
+tippy(".btn-hardcover", {
+  html: "#hardcover-book",
+  arrow: true,
+  animation: "shift",
+  size: "regular",
+  theme: "au",
+  distance: 15,
+});
